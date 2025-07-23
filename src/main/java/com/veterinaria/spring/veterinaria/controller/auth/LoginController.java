bda19e0ef178d30c1cf3fea62d2340980291c04e
JordySC <cjordy2001@gmail.com>
2025-07-23 12:43:53 -0500

    Cambios

--- src/main/java/com/veterinaria/spring/veterinaria/controller/auth/LoginController.java
+++ src/main/java/com/veterinaria/spring/veterinaria/controller/auth/LoginController.java
@@ -1,11 +1,14 @@
 package com.veterinaria.spring.veterinaria.controller.auth;
 
-import com.veterinaria.spring.veterinaria.model.Usuario;
-import com.veterinaria.spring.veterinaria.repository.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.veterinaria.spring.veterinaria.model.Usuario;
+import com.veterinaria.spring.veterinaria.repository.UsuarioRepository;
 
 @Controller
 public class LoginController {
