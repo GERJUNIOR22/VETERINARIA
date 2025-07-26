99f1e6f980d15ce94f241dc7f2a027648340a651
GERJUNIOR22 <121823750+GERJUNIOR22@users.noreply.github.com>
2025-07-25 21:08:51 -0500

    solucion de registro

--- src/main/java/com/veterinaria/spring/veterinaria/controller/RegistroController.java
+++ src/main/java/com/veterinaria/spring/veterinaria/controller/RegistroController.java
@@ -1,26 +1,31 @@
 package com.veterinaria.spring.veterinaria.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-
 import com.veterinaria.spring.veterinaria.model.Usuario;
+import com.veterinaria.spring.veterinaria.repository.UsuarioRepository;
 
 @Controller
 public class RegistroController {
 
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
     @GetMapping("/registro")
     public String mostrarFormularioRegistro(Model model) {
         model.addAttribute("usuario", new Usuario());
-        return "registro"; // Va a buscar registro.html en templates
+        return "public/registro";
     }
 
     @PostMapping("/registro")
     public String procesarRegistro(@ModelAttribute Usuario usuario) {
-        // Aquí deberías guardar el usuario en la BD (si quieres hacerlo)
-        System.out.println("Registrado: " + usuario.getUsername());
-        return "redirect:/login"; // Redirige a login.html al terminar
+        usuario.setRol("USER"); // Asigna rol si es obligatorio
+        usuario.setEstado(Usuario.EstadoUsuario.ACTIVO); // Asigna estado si es obligatorio
+        usuarioRepository.save(usuario); // Guarda el usuario en la BD
+        return "redirect:/login";
     }
 }
